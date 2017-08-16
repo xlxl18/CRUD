3f83dcbb0b42cbb5e6529b6a93716607507de97a
xlxl18 <tima_93@list.ru>
2017-08-16 15:06:33 +0600

    Update UserBase.java

--- src/UserBase.java
+++ src/UserBase.java
@@ -10,23 +10,29 @@ public class UserBase {
     private static ArrayList<User> list = new ArrayList<User>();
 
 
-    private static void  createUser() throws IOException {
-        String name, surname, login, password, number;
-        System.out.println("Введите имя:");
-        name = reader.readLine();
-        System.out.println("Введите фамилию:");
-        surname = reader.readLine();
-        System.out.println("Введите номер телефона:");
-        number = reader.readLine();
-        System.out.println("Введите логин:");
-        login = reader.readLine();
-        System.out.println("Введите пароль:");
-        password = reader.readLine();
-        User user = new User(name, surname, number, login, password);
-        list.add(user);
-        System.out.print("Пользователь: ");
-        System.out.print(user.getName());
-        System.out.println(" успешно создан");
+    private static void  createUser()  {
+        try {
+            String name, surname, login, password, number;
+            System.out.println("Введите имя:");
+            name = reader.readLine();
+            System.out.println("Введите фамилию:");
+            surname = reader.readLine();
+            System.out.println("Введите номер телефона:");
+            number = reader.readLine();
+            System.out.println("Введите логин:");
+            login = reader.readLine();
+            System.out.println("Введите пароль:");
+            password = reader.readLine();
+            User user = new User(name, surname, number, login, password);
+            list.add(user);
+            System.out.print("Пользователь: ");
+            System.out.print(user.getName());
+            System.out.println(" успешно создан");
+        }
+
+        catch (IOException e){
+
+        }
 
     }
 
@@ -43,31 +49,44 @@ public class UserBase {
     }
 
 
-    private static void removeUser() throws IOException {
-        System.out.println("Введите имя пользователя, которого хотите удалить.");
-        String nameForDelete = reader.readLine();
-        for (Iterator<User> iterator = list.iterator(); iterator.hasNext(); )
-            if (iterator.next().getName().equals(nameForDelete)) iterator.remove();
+    private static void removeUser() {
+        try {
+            System.out.println("Введите имя пользователя, которого хотите удалить.");
+
+            String nameForDelete = reader.readLine();
+            for (Iterator<User> iterator = list.iterator(); iterator.hasNext(); )
+                if (iterator.next().getName().equals(nameForDelete)) iterator.remove();
+        }
+
+        catch (IOException e) {
+
+        }
     }
 
 
-    private static void modifyUser()throws IOException{
-        System.out.println("Введите имя пользователя, которого хотите редактировать.");
-        String nameForModify = reader.readLine();
-        for (User object : list) {
-            if (nameForModify.equals(object.getName())) {
-                System.out.println("Введите имя:");
-                object.setName(reader.readLine());
-                System.out.println("Введите фамилию:");
-                object.setSurname(reader.readLine());
-                System.out.println("Введите номер телефона:");
-                object.setNumber(reader.readLine());
-                System.out.println("Введите логин:");
-                object.setLogin(reader.readLine());
-                System.out.println("Введите пароль:");
-                object.setPassword(reader.readLine());
+    private static void modifyUser(){
+        try {
+            System.out.println("Введите имя пользователя, которого хотите редактировать.");
+            String nameForModify = reader.readLine();
+            for (User object : list) {
+                if (nameForModify.equals(object.getName())) {
+                    System.out.println("Введите имя:");
+                    object.setName(reader.readLine());
+                    System.out.println("Введите фамилию:");
+                    object.setSurname(reader.readLine());
+                    System.out.println("Введите номер телефона:");
+                    object.setNumber(reader.readLine());
+                    System.out.println("Введите логин:");
+                    object.setLogin(reader.readLine());
+                    System.out.println("Введите пароль:");
+                    object.setPassword(reader.readLine());
 
+                }
             }
+        }
+
+        catch(IOException e){
+
         }
     }
 
@@ -99,7 +118,7 @@ public class UserBase {
                         System.exit(0);
                         break;
                     default:
-                        System.out.println("Вы ввели неправильный номер команды.");
+                        System.out.println("Вы ввели неправильный номер команды. Пожалуйста попробуйте еще раз!");
                         flag=false;
 
                 }
